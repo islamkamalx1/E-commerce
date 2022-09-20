7939fbd0653f210b525532185f1e68e9bd87fd66
Islam Kamal <islamkamalx1>
2022-09-20 15:51:04 +0200

    edit the responsive and the toggle menu

--- js/index.js
+++ js/index.js
@@ -1,14 +1,17 @@
 const bar = document.getElementById('bar');
 const closeBtn = document.getElementById('close');
 const nav = document.getElementById('nav-items');
+const html = document.querySelector("html");
+const listItems = nav.querySelectorAll('li');
+bar.addEventListener('click', () => {
+    nav.classList.add('active')
+});
 
-if (bar) {
-    bar.addEventListener('click', () => {
-        nav.classList.add('active')
-    })
-}
-if (closeBtn) {
-    closeBtn.addEventListener('click', () => {
-        nav.classList.remove('active');
-    })
-}
+closeBtn.addEventListener('click', () => {
+    nav.classList.remove('active');
+});
+
+html.addEventListener("click", function (e) {
+    if (e.target !== bar && e.target !== nav)
+        nav.classList.remove("active");
+});
