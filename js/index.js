b9403129a57482383e15335c295d0a5d958d7928
Islam Kamal <islamkamalx1>
2022-09-21 23:30:46 +0200

    clickable product

--- js/index.js
+++ js/index.js
@@ -3,6 +3,15 @@ const closeBtn = document.getElementById('close');
 const nav = document.getElementById('nav-items');
 const html = document.querySelector("html");
 const listItems = nav.querySelectorAll('li');
+const mainImage = document.getElementById('main-img');
+const smallImages = document.querySelectorAll('.small-img');
+
+smallImages.forEach(img => {
+    img.addEventListener('click',() => {
+        mainImage.src = img.src;
+    });
+});
+
 bar.addEventListener('click', () => {
     nav.classList.add('active')
 });
@@ -14,4 +23,9 @@ closeBtn.addEventListener('click', () => {
 html.addEventListener("click", function (e) {
     if (e.target !== bar && e.target !== nav)
         nav.classList.remove("active");
+});
+
+const products = document.querySelectorAll('.prod');
+products.forEach(product => {
+    product.addEventListener('click',() => window.location.href = 'sProduct.html');
 });
--- js/sProd.js
+++
@@ -1,8 +0,0 @@
-const mainImage = document.getElementById('main-img');
-const smallImages = document.querySelectorAll('.small-img');
-
-smallImages.forEach(img => {
-    img.addEventListener('click',() => {
-        mainImage.src = img.src;
-    });
-});
